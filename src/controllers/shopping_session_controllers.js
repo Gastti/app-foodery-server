18b611721f322ee079ff891041faf6f5a2e03274
Gastti <gutigas96@gmail.com>
2023-03-14 12:53:27 -0300

    chore: send shopping session id to handle it in token

--- src/controllers/shopping_session_controllers.js
+++ src/controllers/shopping_session_controllers.js
@@ -1,21 +1,18 @@
 const Shopping_Session = require("../models").Shopping_Session;
 
 async function generateShoppingSession(user_id) {
+    try {
+        const findShoppingSession = await Shopping_Session.findOne({ where: { user_id } });
+        const shoppingSession = findShoppingSession
+            ? findShoppingSession
+            : await Shopping_Session.create({ user_id, total: 0, shoppingSessionId: user_id });
 
-    const findShoppingSession = await Shopping_Session.findOne({ where: { user_id } });
-    const shoppingSession = findShoppingSession
-        ? findShoppingSession
-        : await Shopping_Session.create({ user_id, total: 0, shoppingSessionId: user_id });
-
-    // if (!findShoppingSession) {
-    //     Shopping_Session.create({
-    //         user_id,
-    //         total: 0
-    //     });
-    // }
-    
-    console.log('SHOPPING SESSION CONTROLLERS', shoppingSession.id);
-    return shoppingSession.id;
+        console.log('SHOPPING SESSION CONTROLLERS', shoppingSession.id);
+        return shoppingSession.id;
+    } catch (error) {
+        console.log(error);
+        return null
+    }
 }
 
 
