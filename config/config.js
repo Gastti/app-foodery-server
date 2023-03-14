0bb90936cff12369a3685307565ceb110067dff0
Gastti <gutigas96@gmail.com>
2023-03-13 21:41:31 -0300

    feat: add secret key

--- config/config.js
+++ config/config.js
@@ -1,28 +1,33 @@
 require('dotenv').config()
 
 module.exports = {
-    "development": {
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASSWORD,
-        "database":  process.env.DB_NAME,
-        "host": process.env.DB_HOST,
-        "port": process.env.DB_PORT,
-        "dialect": "mysql",
-        "logging": false
+    "database": {
+        "development": {
+            "username": process.env.DB_USER,
+            "password": process.env.DB_PASSWORD,
+            "database": process.env.DB_NAME,
+            "host": process.env.DB_HOST,
+            "port": process.env.DB_PORT,
+            "dialect": "mysql",
+            "logging": false
+        },
+        "test": {
+            "username": process.env.DB_USER,
+            "password": process.env.DB_PASSWORD,
+            "database": process.env.DB_NAME,
+            "host": process.env.DB_HOST,
+            "dialect": "mysql",
+            "logging": false
+        },
+        "production": {
+            "username": process.env.DB_USER,
+            "password": process.env.DB_PASSWORD,
+            "database": process.env.DB_NAME,
+            "host": process.env.DB_HOST,
+            "dialect": "mysql"
+        }
     },
-    "test": {
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_NAME,
-        "host": process.env.DB_HOST,
-        "dialect": "mysql",
-        "logging": false
-    },
-    "production": {
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_NAME,
-        "host": process.env.DB_HOST,
-        "dialect": "mysql"
+    "server": {
+        "SECRET_KEY": process.env.SECRET_KEY
     }
 }
