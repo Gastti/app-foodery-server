8cb0049f9613ef591c31c83e2a8e2587f308cb93
Gastti <gutigas96@gmail.com>
2023-03-17 21:15:21 -0300

    chore: add soft delete

--- migrations/20230317215704-create-role.js
+++ migrations/20230317215704-create-role.js
@@ -1,20 +1,19 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
-
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Carts', {
+    await queryInterface.createTable('Roles', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      user_id: {
-        type: Sequelize.INTEGER
+      name: {
+        type: Sequelize.STRING
       },
-      total: {
-        type: Sequelize.INTEGER
+      desc: {
+        type: Sequelize.STRING
       },
       deletedAt: {
         type: Sequelize.DATE
@@ -31,6 +30,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Carts');
+    await queryInterface.dropTable('Roles');
   }
 };
