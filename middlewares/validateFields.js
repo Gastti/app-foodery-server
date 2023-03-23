df164f1e75c17633d208ded8746c882210b50afd
Gastti <gutigas96@gmail.com>
2023-03-22 21:36:33 -0300

    refactor: change export method

--- middlewares/validateFields.js
+++ middlewares/validateFields.js
@@ -1,6 +1,6 @@
 const { validationResult } = require('express-validator');
 
-const validateFields = (req, res, next) => {
+function validateFields(req, res, next) {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json(errors)
